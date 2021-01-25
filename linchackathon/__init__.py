7bebd72940e2116b85f41cde917250796647ff5a
YasserMahfoud <yassermahfoud1995@gmail.com>
2021-01-26 00:09:41 +0100

    added the hackathon code

--- linchackathon/__init__.py
+++ linchackathon/__init__.py
@@ -1,5 +1,44 @@
-"""Top-level package for linchackathon."""
+# -*- coding: utf-8 -*-
 
-__author__ = """Yasser Mahfoud"""
-__email__ = 'yassermahfoud1995@gmail.com'
-__version__ = '0.1.0'
+"""
+LINC
+======
+
+
+
+LINC is short for I don't know actually
+
+
+"""
+
+from . import auth
+from . import private
+from . import public
+from . import orders
+from . import stoploss
+
+#auth.py
+init = auth.init
+
+#public.py
+getTickers = public.getTickers
+getTickersNames = public.getTickersNames
+getStock = public.getStock
+getStockHistory = public.getStockHistory
+
+#private.py
+buyStock = private.buyStock
+sellStock = private.sellStock
+getPortfolio = private.getPortfolio
+getSaldo = private.getSaldo
+getHistory = private.getHistory
+
+#orders.py
+placeOrder= orders.placeOrder
+getOrders= orders.getOrders
+deleteOrder= orders.deleteOrder
+
+#orders.py
+getStoplosses= stoploss.getStoplosses
+placeStoploss= stoploss.placeStoploss
+deleteStoploss= stoploss.deleteStoploss
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ['Click>=7.0', 'requests>=2.24.0', 'pandas>=1.2.0', 'numpy>=1.19.2']
 
 setup_requirements = [ ]
 
