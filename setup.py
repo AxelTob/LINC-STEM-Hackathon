2d554c1b1bbea187d80ce9775c3e09e99ad7eb71
YasserMahfoud <yassermahfoud1995@gmail.com>
2021-01-26 00:47:15 +0100

    Changed pandas requirement

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'requests>=2.0.0', 'pandas>=1.0.0', 'numpy>=1.18.0']
+requirements = ['Click>=7.0', 'requests>=2.0.0', 'pandas>=0.25.3', 'numpy>=1.18.0']
 
 setup_requirements = [ ]
 
