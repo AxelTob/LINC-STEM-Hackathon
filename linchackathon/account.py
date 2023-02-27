53ca125abcb9a18fe9f87880262a373b0528dee7
Axel <ax2070to-s@student.lu.se>
2023-02-28 00:34:11 +0100

    refactor

--- linchackathon/account.py
+++ linchackathon/account.py
@@ -1,102 +1,62 @@
-# =============================================================================
-#  Imports
-# =============================================================================
 import requests
-from . import ipaddr as u
-
-# =============================================================================
-# Get orders
-# =============================================================================
+from . import ipaddr as ip_address
 
 
-def get_orders():
+def get_all_orders():
     """
-    This function returns a list of all the order that you have placed
-    (completed and active)
+    Returns a list of all completed and pending orders
     """
-
     return get_completed_orders() + get_pending_orders()
-
-# =============================================================================
-# Get completed orders
-# =============================================================================
 
 
 def get_completed_orders():
     """
-    This function returns a list of all the order that are completed
+    Returns a list of completed orders
     """
-
-    url_g = u.url + '/account/get_completed_orders'
-    body = {"api_key": u.token}
+    url = ip_address.url + '/account/get_completed_orders'
+    body = {"api_key": ip_address.token}
     with requests.Session() as session:
-        get = session.get(url_g, json=body)
-
-    return get.json()
-
-
-# =============================================================================
-# Get pending orders
-# =============================================================================
-
-def get_pending_orders():
-    """
-    This function returns a list of all the order that are completed
-    """
-
-    url_g = u.url + '/account/open_orders'
-    body = {"api_key": u.token}
-    with requests.Session() as session:
-        get = session.get(url_g, json=body)
-
-    return get.json()
-
-
-# =============================================================================
-# Get stoploss orders
-# =============================================================================
-
-def get_stoploss_orders():
-    """
-    This function returns a list of all the order that are completed
-    """
-
-    url_g = u.url + '/account/get_stoploss_orders'
-    body = {"api_key": u.token}
-    with requests.Session() as session:
-        get = session.get(url_g, json=body)
-
-    return get.json()
-
-
-# =============================================================================
-# get saldo
-# =============================================================================
-
-def get_saldo():
-    """
-    This function returns an integer representing your current balance
-    """
-
-    url_g = u.url + f'/account/saldo'
-    body = {"api_key": u.token}
-    response = requests.get(url_g, json=body)
-
+        response = session.get(url, json=body)
     return response.json()
 
 
-# =============================================================================
-# get portfolio
-# =============================================================================
+def get_pending_orders():
+    """
+    Returns a list of pending orders
+    """
+    url = ip_address.url + '/account/open_orders'
+    body = {"api_key": ip_address.token}
+    with requests.Session() as session:
+        response = session.get(url, json=body)
+    return response.json()
+
+
+def get_stoploss_orders():
+    """
+    Returns a list of stoploss orders
+    """
+    url = ip_address.url + '/account/get_stoploss_orders'
+    body = {"api_key": ip_address.token}
+    with requests.Session() as session:
+        response = session.get(url, json=body)
+    return response.json()
+
+
+def get_balance():
+    """
+    Returns an integer representing the current balance
+    """
+    url = ip_address.url + f'/account/saldo'
+    body = {"api_key": ip_address.token}
+    response = requests.get(url, json=body)
+    return response.json()
+
 
 def get_portfolio():
     """
-    This function returns a dictionary that contains the amount of shares you 
-    own from each stock.
+    Returns a dictionary with the amount of shares owned for each stock
     """
-
-    url_g = u.url + '/account/portfolio'
-    body = {"api_key": u.token}
-    response = requests.get(url_g, json=body)
-
+    url = ip_address.url + '/account/portfolio'
+    body = {"api_key": ip_address.token}
+    response = requests.get(url, json=body)
     return response.json()
