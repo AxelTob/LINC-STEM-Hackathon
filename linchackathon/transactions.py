93363e56ec544ed66751d99c67d182cad57329ee
Axel <ax2070to-s@student.lu.se>
2023-02-28 02:17:59 +0100

    change to ticker as term

--- linchackathon/transactions.py
+++ linchackathon/transactions.py
@@ -14,13 +14,13 @@ from typing import Union, Dict
 # =============================================================================
 
 
-def _place_order(order_type: str, symbol: str, amount: int, price: Union[int, None] = None, days_to_cancel: int = 30) -> Dict:
+def _place_order(order_type: str, ticker: str, amount: int, price: Union[int, None] = None, days_to_cancel: int = 30) -> Dict:
     """
     This is a private function for placing buy, sell, and stoploss orders for securities.
 
     Args:
         order_type (str): The type of order (buy, sell, or stoploss)
-        symbol (str): A security symbol (ex: STOCK1, STOCK2, etc.)
+        ticker (str): A security ticker (ex: STOCK1, STOCK2, etc.)
         amount (int): Number of shares
         price (int, optional): The price for which you want the security to be under in order to buy. If not specified, the order will be placed at the current price. Defaults to None.
         days_to_cancel (int, optional): How many days you want this trade to stay active: e.g entering 30 means that trade will be held for 30 days and then cancelled if security price never reaches given price to buy. Defaults to None.
@@ -33,7 +33,7 @@ def _place_order(order_type: str, symbol: str, amount: int, price: Union[int, No
         raise ValueError("""The amount and price must be integers""")
 
     params = {'type': order_type,
-              'symbol': symbol,
+              'ticker': ticker,
               'amount': amount,
               'days_to_cancel': days_to_cancel}
     body = {'api_key': u.token}
@@ -48,12 +48,12 @@ def _place_order(order_type: str, symbol: str, amount: int, price: Union[int, No
     return response.json()
 
 
-def buy(symbol: str, amount: int, price: Union[int, None] = None, days_to_cancel: int = 30) -> Dict:
+def buy(ticker: str, amount: int, price: Union[int, None] = None, days_to_cancel: int = 30) -> Dict:
     """
     This function places a buy order for a given security.
 
     Args:
-        symbol (str): A security symbol (ex: STOCK1, STOCK2, etc.)
+        ticker (str): A security ticker (ex: STOCK1, STOCK2, etc.)
         amount (int): Number of shares
         price (int, optional): The price for which you want the security to be under in order to buy. If not specified, the order will be placed at the current price. Defaults to None.
         days_to_cancel (int, optional): How many days you want this trade to stay active: e.g entering 30 means that trade will be held for 30 days and then cancelled if security price never reaches given price to buy. Defaults to None.
@@ -61,15 +61,15 @@ def buy(symbol: str, amount: int, price: Union[int, None] = None, days_to_cancel
     Returns:
         dict: The response content from the server as a dictionary
     """
-    return _place_order('buy', symbol, amount, price, days_to_cancel)
+    return _place_order('buy', ticker, amount, price, days_to_cancel)
 
 
-def sell(symbol: str, amount: int, price: Union[int, None] = None, days_to_cancel: int = 30) -> Dict:
+def sell(ticker: str, amount: int, price: Union[int, None] = None, days_to_cancel: int = 30) -> Dict:
     """
     This function places a sell order for a given security.
 
     Args:
-        symbol (str): A security symbol (ex: STOCK1, STOCK2, etc.)
+        ticker (str): A security ticker (ex: STOCK1, STOCK2, etc.)
         amount (int): Number of shares
         price (int, optional): The price for which you want the security to be under in order to buy. If not specified, the order will be placed at the current price. Defaults to None.
         days_to_cancel (int, optional): How many days you want this trade to stay active: e.g entering 30 means that trade will be held for 30 days and then cancelled if security price never reaches given price to buy. Defaults to None.
@@ -79,15 +79,15 @@ def sell(symbol: str, amount: int, price: Union[int, None] = None, days_to_cance
 
 
     """
-    return _place_order('sell', symbol, amount, price, days_to_cancel)
+    return _place_order('sell', ticker, amount, price, days_to_cancel)
 
 
-def stoploss(symbol: str, amount: int, price: Union[int, None] = None, days_to_cancel: int = 30):
+def stoploss(ticker: str, amount: int, price: Union[int, None] = None, days_to_cancel: int = 30):
     """
     This function places a sell order for a given security.
 
     Args:
-        symbol (str): A security symbol (ex: STOCK1, STOCK2, etc.)
+        ticker (str): A security ticker (ex: STOCK1, STOCK2, etc.)
         amount (int): Number of shares
         price (int, optional): The price for which you want the security to be under in order to buy. If not specified, the order will be placed at the current price. Defaults to None.
         days_to_cancel (int, optional): How many days you want this trade to stay active: e.g entering 30 means that trade will be held for 30 days and then cancelled if security price never reaches given price to buy. Defaults to None.
@@ -95,30 +95,30 @@ def stoploss(symbol: str, amount: int, price: Union[int, None] = None, days_to_c
     Returns:
         dict: The response content from the server as a dictionary
     """
-    return _place_order('stoploss', symbol, amount, price, days_to_cancel)
+    return _place_order('stoploss', ticker, amount, price, days_to_cancel)
 
 
-def cancel(order_id: Union[int, None] = None, symbol: Union[str, None] = None):
+def cancel(order_id: Union[int, None] = None, ticker: Union[str, None] = None):
     """
     This function cancels a specific order or all orders for a given security.
 
     Args:
         order_id (int, optional): The id of the order you want to cancel. Defaults to None.
-        symbol (str, optional): The symbol of the stock you want to cancel all orders for. Defaults to None.
+        ticker (str, optional): The ticker of the stock you want to cancel all orders for. Defaults to None.
 
     Returns:
         str: The response content from the server as a string
     """
 
-    if order_id is None and symbol is None:
-        raise ValueError("""You must specify either an order_id or a symbol""")
+    if order_id is None and ticker is None:
+        raise ValueError("""You must specify either an order_id or a ticker""")
 
     params = {'api_key': u.token}
 
     if order_id is not None:
         params['order_id'] = order_id
     else:
-        params['symbol'] = symbol
+        params['ticker'] = ticker
 
     url_s = u.url + '/cancel_order'
 
